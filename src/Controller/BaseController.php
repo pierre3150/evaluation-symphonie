ff420c0dbf6ef40781a6db295e09910af021830f
pierre3150 <pierre.parain315@gmail.com>
2024-10-01 11:23:55 +0200

    ça avance

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -7,10 +7,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BaseController extends AbstractController
 {
-    #[Route('/', name: 'app_acceuil')]
+    #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
         return $this->render('base/index.html.twig', [
+        ]);
+    }
+
+    #[Route('/ajoutBonbon', name: 'app_ajout_bonbon')]
+    public function ajoutBonbon(): Response
+    {
+        return $this->render('base/ajoutBonbon.html.twig', [
+
         ]);
     }
 }
---
+++ src/Entity/TypeBonbon.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TypeBonbonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TypeBonbonRepository::class)]
+class TypeBonbon
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $designation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDesignation(): ?string
+    {
+        return $this->designation;
+    }
+
+    public function setDesignation(string $designation): static
+    {
+        $this->designation = $designation;
+
+        return $this;
+    }
+}
